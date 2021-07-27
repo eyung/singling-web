fb1712d4f2eeb29265d44aff448572b1682d4ebe
eyung <effiamyung@gmail.com>
2021-07-27 16:42:06 -0700

    start!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
+import React, {Component, useState, useEffect} from "react";
+import Button from '@material-ui/core/Button';
 import './App.css';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const UserProfiles = () => {
+
+  const [userProfiles, setUserProfiles] = useState([]);
+  
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
+      console.log(res);
+
+      const data = res.data;
+      setUserProfiles(res.data);
+    });
+  }
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return userProfiles.map((userProfile, index) => {
+    return (
+      <div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+      </div>
+    )
+  });
+
+}
+
+//const websiteURL = "http://localhost:8080/"
+const websiteURL = "http://3.99.30.184:8080/"
+const s3URL = "https://effiam-bucket.s3.ca-central-1.amazonaws.com/audio/"
+
+class App extends Component {
+
+  constructor () {
+    super()
+
+    this.state = {
+      inputValue: ''
+    };
+
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+  }
+
+  handleClick () {
+    console.log('Success!')
+
+    const testData = 
+      { audioID: uuidv4(), audioLink: 'this is a test' }
+    ;
+
+    axios.get('http://localhost:8080/api/v1/audio-profile/test')
+    .then(
+      res => { console.log(res.data)
+    });
+  }
+
+  handleClick2 () {
+    
+    console.log("Processing text: " + this.state.inputValue)
+
+    axios.post(websiteURL + 'api/v1/audio-profile/test3', { 
+      textID: uuidv4(), textData: this.state.inputValue })
+    .then( res => { 
+      //console.log(res.data.audioLink)
+      console.log("playing audio file: " + s3URL + res.data.audioLink)
+      this.audio = new Audio(s3URL + res.data.audioLink)
+      this.audio.load()
+      this.playAudio()
+    });
+
+    
+
+  }
+
+  playAudio() {
+    const audioPromise = this.audio.play()
+    if (audioPromise !== undefined) {
+      audioPromise
+        .then(_ => {
+          // autoplay started
+        })
+        .catch(err => {
+          // catch dom exception
+          console.info(err)
+        })
+    }
+}
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value 
+    });
+  }
+  
+  
+  render () {
+    return (
+      <div className='app'>
+
+        <div class='top-bar'>
+          <h1>Singling Web</h1>
+        </div>
+          
+        
+        <div class='main-container'>
+          <textarea id='userTextArea' value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            className='button' 
+            onClick={this.handleClick2}
+          >
+            Play
+          </Button>
+
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
